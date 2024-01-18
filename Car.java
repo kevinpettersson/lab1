7330177fac70771e8b296ade81d168da4a6608aa
kevinpettersson <kevin.petterssons@hotmail.com>
2024-01-18 16:01:12 +0100

    Changes by ottow

--- Car.java
+++ Car.java
@@ -2,11 +2,13 @@ import java.awt.*;
 import java.lang.Math;
 public abstract class Car {
 
-    final int nrDoors;
-    final double enginePower;
-    double currentSpeed;
-    Color color;
-    final String modelName;
+    final int nrDoors;              //Number of door will never change for a specific car
+    final double enginePower;       //Engine power is a set number
+    double currentSpeed;            //Speed is a variable that can change over time
+    Color color;                    //Color can change as well
+    final String modelName;         //A Volvo240 will always be a Volvo240
+    int[] position;                 //The current position of the car
+    int heading;
 
     public Car (int nrDoors, double enginePower, Color color, String modelName) {
 
@@ -15,8 +17,22 @@ public abstract class Car {
         this.currentSpeed = currentSpeed;
         this.color = color;
         this.modelName = modelName;
+        this.position = new int[]{0,0};
+        this.heading = 0;
     }
 
+    public int turnLeft2(int degree) {
+        return Math.abs((heading - degree)% 360);
+    }
+
+    public int turnRight2(int degree) {
+        return Math.abs((heading + degree) % 360);
+    }
+
+   public void move() {
+       this.position[0] = (int) (this.position[0] + Math.cos(heading)*currentSpeed);
+       this.position[1] = (int) (this.position[1] + Math.cos(heading)*currentSpeed);
+   }
 
     public String getModelName(){
         return modelName;
