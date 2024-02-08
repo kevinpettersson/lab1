c02b3ec114154bf0d4a03172ddf358164170b45e
kevinpettersson <kevin.petterssons@hotmail.com>
2024-02-08 13:53:11 +0100

    Fixat packages och att allt ligger i samma repo

---
+++ test/lab1/ScaniaTest.java
@@ -0,0 +1,43 @@
+package lab1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import lab1.*;
+class ScaniaTest {
+
+    @Test
+    void getTruckBedAngle() {
+
+    }
+
+    @Test
+    void raiseTruckbed() {
+        Scania scania = new Scania();
+        scania.transform.currentSpeed = 0;
+        scania.setTruckBedAngle(60);
+        scania.raiseTruckBed(20);
+        assertTrue(scania.getTruckBedAngle() == 40);
+    }
+
+    @Test
+    void lowerTruckbed() {
+        Scania scania = new Scania();
+        scania.transform.currentSpeed = 0;
+        scania.setTruckBedAngle(60);
+        scania.lowerTruckBed(70);
+        assertTrue(scania.getTruckBedAngle() == 70);
+    }
+
+
+    @Test
+    void speedFactor() {
+    }
+
+    @Test
+    void incrementSpeed() {
+    }
+
+    @Test
+    void decrementSpeed() {
+    }
+}
